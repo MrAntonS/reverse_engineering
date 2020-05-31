fbc571bd76833758fa1a80c78aeacb4bd5bda0e2
Tatsua <45099803+MrAntonS@users.noreply.github.com>
2020-05-31 16:30:22 +0300

    Add files via upload
    Сама игра и код к ней если кто совсем того

---
+++ ConsoleApplication54.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    char ch;
+    int code;
+    int hp = 100;
+    int *p_hp = &hp;
+    cout << p_hp << endl;
+    while (hp > 0)
+    {
+        ch = _getch();
+        code = static_cast<int>(ch);
+        if (ch == 32) {
+            hp = hp - 15;
+            cout << "Your hp now is " << hp << '\n';
+        }
+    }
+    int x;
+    cin >> x;
+}
